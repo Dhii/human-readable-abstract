95f288e0de30b66ce121eae04e931b86e18a1013
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-11 16:01:16 +0200

    Added `LabelAwareTrait`

---
+++ test/functional/LabelAwareTraitTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Dhii\Util\String\FuncTest;
+
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \Dhii\Util\String\LabelAwareTrait}.
+ *
+ * @since [*next-version*]
+ */
+class LabelAwareTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Util\String\LabelAwareTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    public function createInstance()
+    {
+        // Create mock
+        $mock = $this->getMockForTrait(
+            static::TEST_SUBJECT_CLASSNAME,
+            [],
+            '',
+            true,
+            true,
+            true,
+            []
+        );
+
+        return $mock;
+    }
+
+    /**
+     * Tests the label getter and setter methods to ensure correct value assignment and retrieval.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetSetLabel()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $label = 'foobar testing string';
+        $reflect->_setLabel($label);
+
+        $this->assertEquals($label, $reflect->_getLabel());
+    }
+}
